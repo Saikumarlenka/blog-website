5193c4109d5b63241764c8429e7ac2470e9fe56e
Saikumarlenka <saikumarlenka123@gmail.com>
2024-01-31 16:03:53 +0530

    complete update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,36 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  currentUser:null,
-  error:null,
-  loading:false
-}
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
-export const userSlice = createSlice({
+const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     signInStart: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.loading = true;
-      state.error=null
+      state.error = null;
     },
-    signInSuccess: (state,action) => {
-      state.currentUser=action.payload;
-      state.loading=false;
-      state.error=null;
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
     },
-    signInFailure: (state,action) => {
-        state.loading=false
-        state.error = action.payload
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
     },
   },
-})
+});
 
-// Action creators are generated for each case reducer function
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
